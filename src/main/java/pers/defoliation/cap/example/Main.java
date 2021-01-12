b315cc31413b83aebd4a25ccfedaa71670131383
defoli_ation <defoli_ation@outlook.com>
2021-01-12 20:00:30 +0800

    update

--- src/main/java/pers/defoliation/cap/example/Main.java
+++ src/main/java/pers/defoliation/cap/example/Main.java
@@ -1,10 +1,10 @@
 package pers.defoliation.cap.example;
 
-import engine.command.argument.FunctionSenderArgument;
+import engine.command.argument.FunctionArgument;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import pers.defoliation.cap.CapCommandManager;
-import pers.defoliation.cap.example.stage2.Commands;
+import pers.defoliation.cap.example.stage1.Commands;
 import pers.defoliation.cap.example.stage2.Home;
 import pers.defoliation.cap.example.stage2.HomeManager;
 
@@ -17,11 +17,11 @@ public class Main extends JavaPlugin {
         CapCommandManager capCommandManager = new CapCommandManager(this);
 
         capCommandManager.getArgumentManager()
-                .appendArgument(
-                        FunctionSenderArgument.getBuilder(Home.class,"home")
-                        .setSuggester((commandSender, s, strings) -> HomeManager.INSTANCE.getPlayerHomes((Player) commandSender))
-                        .setParse((sender,argument) -> Optional.ofNullable(HomeManager.INSTANCE.getHome((Player) sender,argument)))
-                        .get()
+                .setClassDefaultArgument(
+                        FunctionArgument.getBuilder(Home.class, "home")
+                                .setSuggester((commandSender, s, strings) -> HomeManager.INSTANCE.getPlayerHomes((Player) commandSender))
+                                .setParse((sender, argument) -> Optional.ofNullable(HomeManager.INSTANCE.getHome((Player) sender, argument)))
+                                .get()
                 );
 
         capCommandManager.getBuilder()
